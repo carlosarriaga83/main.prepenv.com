5428ea74aebf1498438a8df9d8124c69518ec7ef
carlosarriaga83 <cear83@gmail.com>
2025-06-19 21:14:56 -0400

    Update config.php

--- PHP/config.php
+++ PHP/config.php
@@ -14,7 +14,7 @@
 //////////////// DATABASE   /////////////
 
 	$DB_CREDENTIALS = [];
-	$DB_CREDENTIALS[0]['HOST'] = 'localhost';
+	$DB_CREDENTIALS[0]['HOST'] = '193.203.166.205';
 	$DB_CREDENTIALS[0]['USER'] = 'u124132715_db';
 	$DB_CREDENTIALS[0]['PWD'] 	= 'Pellu8aa';
 	$DB_CREDENTIALS[0]['DB'] 	= 'u124132715_DB';
@@ -23,7 +23,7 @@
 //////////////// DATABASE 1  /////////////
 
 
-	$DB_CREDENTIALS[1]['HOST'] = 'localhost';
+	$DB_CREDENTIALS[1]['HOST'] = '193.203.166.205';
 	$DB_CREDENTIALS[1]['USER'] = 'u124132715_sa';
 	$DB_CREDENTIALS[1]['PWD'] 	= 'Pluma123.';
 	$DB_CREDENTIALS[1]['DB'] 	= 'u124132715_curso';
@@ -33,7 +33,7 @@
 //////////////// DATABASE 2  /////////////
 
 
-	$DB_CREDENTIALS[2]['HOST'] = 'localhost';
+	$DB_CREDENTIALS[2]['HOST'] = '193.203.166.205';
 	$DB_CREDENTIALS[2]['USER'] = 'u124132715_io';
 	$DB_CREDENTIALS[2]['PWD'] 	= 'Pellu8aa1!';
 	$DB_CREDENTIALS[2]['DB'] 	= 'u124132715_io';
